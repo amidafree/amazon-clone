8c2ff09ed57f405ca194bb709463427d7782224e
amida <amida.free@gmail.com>
2025-03-14 14:33:03 +0900

    Add files via upload

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,32 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import renderCheckoutHeader from "./checkout/checkoutHeader.js";
-// import '../data/cart-class.js'; //run all code inside this path
-import '../data/backend-practice.js';
+import { loadProducts, loadProductsFetch } from "../data/products.js";
+import { loadCart, loadCartFetch } from "../data/cart.js";
 
-renderOrderSummary();
-renderPaymentSummary();
-renderCheckoutHeader();
+async function loadPage() {
+  try {
+    await Promise.all([
+      loadProductsFetch(),
+      loadCartFetch()
+    ]);
+
+    await loadProductsFetch();
+    await loadCartFetch();
+
+    const value = await new Promise((resolve, reject) => {
+      loadCart(() => {
+        reject('error3');
+      });
+    });
+
+  } catch (error) {
+    console.log('Unexpected error. Please try again later.');
+  }
+
+  renderOrderSummary();
+  renderPaymentSummary();
+  renderCheckoutHeader();
+}
+loadPage();
+
